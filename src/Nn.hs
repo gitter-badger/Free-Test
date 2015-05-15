fa2d52c23fd0746d465fdfa4b6ea9b470bb48142
SugimotoSohei <1130071@stu.hus.ac.jp>
2015-05-15 22:49:46 +0900

    型を追加

--- src/Nn.hs
+++ src/Nn.hs
@@ -1,6 +1,9 @@
 module Nn where
 
-type nn = Int nn
+type Weight = Double
+type Input  = Double
+type Neuron = (Input,Weight)
+type Theta =  Double
 
 fStep :: (Num a,Ord a) => a -> Int
 fStep x
@@ -19,15 +22,15 @@ fSigmoid x = (1) / (1 + exp (-x))
 --(入力（0 or 1）,重み(任意の実数)) * 後で実装 *
 --zip [0,1,1,1] [1.0,0.5,0.4,0.8]
 
-newron :: (Floating a) => (Int,a) -> a
-newron (1,w) = w
-newron _ = 0
+neuron :: Neuron -> Weight
+neuron (1,w) = w
+neuron _ = 0
 
-iNewron :: (Floating a,Ord a) => [(Int,a)] -> a -> Int
-iNewron xs sita =  fStep $ (sum $ map newron $ xs ) - sita
+ineuron :: [Neuron] -> Weight -> Int
+ineuron xs sita =  fStep $ (sum $ map neuron $ xs ) - sita
 
-iAnd x y = iNewron [(x,1.0),(y,1.0)] 1.5
-iOr  x y = iNewron [(x,1.0),(y,1.0)] 0.5
-iNot x = iNewron [(x,(-1.0))] (-0.5)
+iAnd x y = ineuron [(x,1.0),(y,1.0)] 1.5
+iOr  x y = ineuron [(x,1.0),(y,1.0)] 0.5
+iNot x = ineuron [(x,(-1.0))] (-0.5)
   
  -- -> [Int]
