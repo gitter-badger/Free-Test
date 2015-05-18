16a45333e43393fcea07f664d53d0ac710b5fc7b
SugimotoSohei <1130071@stu.hus.ac.jp>
2015-05-18 15:55:14 +0900

    simple perceptron 0.0

--- src/Nn.hs
+++ src/Nn.hs
@@ -1,36 +1,60 @@
 module Nn where
 
-type Weight = Double
-type Input  = Double
-type Neuron = (Input,Weight)
+type InPut  = [Double]
+type Weight = [Double]
 type Theta =  Double
+type Neuron = (InPut,Weight,Theta)
+type OutPut = Double
 
-fStep :: (Num a,Ord a) => a -> Int
+mInPut :: [InPut]
+mInPut = [[1,1,1,0,0],
+          [1,1,1,1,1],
+          [0,0,0,0,0],
+          [0,1,1,1,1],
+          [1,0,0,0,1]]
+
+mTheta = repeat 0.5 -- hold
+oTheta = [0.5]
+
+imWhight = repeat (repeat 1.0) -- hold
+moWhight = [1.0]
+
+-- 伝達関数 G(x)
+
+fStep :: (Num a,Ord a) => a -> a
 fStep x
-  | x >= 0 = 1
-  | x <  0 = 0
+  | x > 0 = 1
+  | x <=  0 = 0
 
-fSign :: (Num a,Ord a) => a -> Int
+fSign :: (Num a,Ord a) => a -> a
 fSign x
-  | x >= 0 = 1
-  | x <  0 = (-1)
+  | x > 0 = 1
+  | x <=  0 = (-1)
 
 fSigmoid :: (Floating a) => a -> a
 fSigmoid x = (1) / (1 + exp (-x))
 
+-- #############################
 
---(入力（0 or 1）,重み(任意の実数)) * 後で実装 *
---zip [0,1,1,1] [1.0,0.5,0.4,0.8]
+verticalNeuron :: [InPut] -> [Weight] -> [Theta] -> [Neuron]
+verticalNeuron [] _ _ = []
+verticalNeuron _ [] _ = []
+verticalNeuron _ _ [] = []
+verticalNeuron (i:is) (w:ws) (t:ts) = (i,w,t):verticalNeuron is ws ts
 
-neuron :: Neuron -> Weight
-neuron (1,w) = w
-neuron _ = 0
+headSum :: InPut -> Weight -> Double
+headSum [] _ = 0
+headSum _ [] = 0
+headSum (i:is) (w:ws) = (i*w) + (headSum is ws)
 
-ineuron :: [Neuron] -> Weight -> Int
-ineuron xs sita =  fStep $ (sum $ map neuron $ xs ) - sita
+outNeuron :: Neuron -> OutPut
+outNeuron (i,w,t) = fSigmoid $ (headSum i w) - t
 
-iAnd x y = ineuron [(x,1.0),(y,1.0)] 1.5
-iOr  x y = ineuron [(x,1.0),(y,1.0)] 0.5
-iNot x = ineuron [(x,(-1.0))] (-0.5)
-  
- -- -> [Int]
+
+mlNeuron i = map outNeuron $ verticalNeuron i imWhight mTheta
+
+olNeuron i w t = outNeuron (i,w,t)
+
+--iAnd x y = outNeuron ([x,y],[1.0,1.0],1.5)
+--iOr  x y = outNeuron ([x,y],[1.0,1.0],0.5)
+--iNot x = outNeuron ([x],[(-1.0)],(-0.5))
